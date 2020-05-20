e2bf974d4d92b038743f72d3bdc3a1968a1330e5
Mrutyunjaya Behera <55201679+MrutyunjayaBehera@users.noreply.github.com>
2020-05-20 08:12:56 +0530

    Update hrank.py

--- hrank.py
+++ hrank.py
@@ -1,47 +1,39 @@
+
+#TASK
+
+#This code is to directly apply at nested-list problem in python in HACKERRANK
+
+#Given the names and grades for each student in a Physics class of N students, 
+#store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+#Note: If there are multiple students with the same grade, 
+#order their names alphabetically and print each name on a new line.
+
+
+
+#CODE
+
 import math
 import os
 import random
 import re
 import sys
 
+if __name__ == '__main__':
+    
+score_list = []
+mark_sheet = []
 
-"""n = input("enter the  number: ")
-n = int(n)
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    mark_sheet.append([name,score])
+    score_list.append(score)
 
-if n % 2 != 0:
-	print("Weird")
-else:
-	if n > 2 and n < 5:
-		print("Not Weird")
-	elif n > 6 and n < 20:
-		print("Weird")
-	elif n > 20:
-		print("Not Weird")"""
+second_lowest_mark = sorted(list(dict.fromkeys(score_list)))[1]    #avoiding duplications and sorting the list
 
-"""n = input("enter the number: ")
-n = int(n)
-for i in range(1,n+1):
-    print(i, end = "")"""
-
-"""x = input("raw_input1: ")
-x = int(x)
-y = input("raw_input2: ")
-y = int(y) 
-z = input("raw_input3: ")
-z = int(z)
-n = input("raw_input4: ")
-n = int(n)
-
-print([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n])"""
-
-k = input()
-arr = []
-arr = k.split(" ")
-print(arr)
-
-large = max(arr)
-n = len(arr)
-for i in range(n):
-	if large == max(arr):
-		arr.remove(max(arr))
-print(max(arr))
+for name,marks in sorted(mark_sheet):
+    if marks == second_lowest_mark:
+        print(name)
+	
+#ENJOY
